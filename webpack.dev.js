4ec29e7e41fac128639f20c1b378955b77db1a22
jake-lonceras <93508511+jake-lonceras@users.noreply.github.com>
2022-03-12 08:20:43 +0800

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -14,7 +14,7 @@ module.exports = {
 
   output:   {
     filename: "[name][contenthash].bundle.js",
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./dist/dev"),
     clean: true,
   },
 
