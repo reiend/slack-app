16119a85abbd89a8332c645ca4b6d6e19ffea289
jake <loncerasjakerei@gmail.com>
2022-03-14 15:48:22 +0800

    Initial commit for chat page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Signin from "@components/Signin.jsx";
 import Signup from "@components/Signup.jsx";
+import Chat from "@components/Chat.jsx";
 
 const App = () => {
   return (
     <div>
       {/* <Signin/> */}
       {/* <Signup/> */}
+      <Chat/>
     </div>
   );
 };
---
+++ src/res/components/Chat.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Chat.scss";
+
+const Chat = () => {
+  return (
+    <main>
+      <h2>chat</h2>
+    </main>
+  );
+};
+
+export default Chat;
+
