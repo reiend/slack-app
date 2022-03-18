e7a433ae029612332b4d0a95a1152560bfde9381
alfredosdpiii <alfredo.sdpiii@gmail.com>
2022-03-19 02:18:55 +0800

    added form validation

--- src/res/components/Signup.jsx
+++ src/res/components/Signup.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import UsappLogo from "@svg/UsappLogo.jsx";
 import ChatCloudSignup from "@svg/ChatCloudSignup.jsx";
 import "./Signup.scss";
 import axios from "axios";
 import { Link } from "react-router-dom";
-const baseURL = "http://206.189.91.54/api/v1/";
+import { useForm } from "react-hook-form";
+// const baseURL = "http://206.189.91.54/api/v1/";
 
 const Signup = () => {
   const [hasInputFirstname, setHasInputFirstname] = useState(false);
@@ -12,7 +13,14 @@ const Signup = () => {
   const [hasInputEmailSignup, setHasInputEmailSignup] = useState(false);
   const [hasInputPasswordSignup, setHasInputPasswordSignup] = useState(false);
   const [hasInputPasswordRetype, setHasInputPasswordRetype] = useState(false);
-
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const password = useRef({});
+  password.current = watch("password", "");
   // Input placeholder animation when focus
   const onChangeInput = (event) => {
     const target = event.target;
@@ -34,25 +42,24 @@ const Signup = () => {
       else setHasInputPasswordRetype(false);
     }
   };
-  const handleSignUp = (e) => {
+  const handleSignUp = (data, e) => {
     e.preventDefault();
     axios
-      .post(`${baseURL}auth?`, {
-        email: e.target.email_signup.value,
-        password: e.target.password_signup.value,
-        password_confirmation: e.target.password_retype.value,
+      .post(`${process.env.BASEURL}auth?`, {
+        email: data.email.value,
+        password: data.password.value,
+        password_confirmation: data.password_confirmation.value,
       })
       .then((res) => {
         console.log(res);
       });
   };
 
-
   return (
     <main className="signup">
       <UsappLogo className="usapp-logo-signup" />
       <h2 className="signup-heading">Sign up to Chat/Usap</h2>
-      <form className="form-signup" onSubmit={handleSignUp}>
+      <form className="form-signup" onSubmit={handleSubmit(handleSignUp)}>
         <div className="firstname">
           <label
             htmlFor="firstname"
@@ -69,7 +76,15 @@ const Signup = () => {
             name="firstname"
             id="firstname"
             onChange={onChangeInput}
+            {...register("firstname", {
+              required: "This is required",
+              maxLength: {
+                value: 30,
+                message: "Whoah there! too many characters!",
+              },
+            })}
           />
+          <p>{errors.firstname?.message}</p>
         </div>
         <div className="lastname">
           <label
@@ -87,7 +102,15 @@ const Signup = () => {
             name="lastname"
             id="lastname"
             onChange={onChangeInput}
+            {...register("lastname", {
+              required: "This is required",
+              maxLength: {
+                value: 30,
+                message: "Whoah there! too many characters!",
+              },
+            })}
           />
+          <p>{errors.lastname?.message}</p>
         </div>
         <div className="email-signup">
           <label
@@ -105,7 +128,9 @@ const Signup = () => {
             name="email_signup"
             id="email-signup"
             onChange={onChangeInput}
+            {...register("email", { required: "an email is required" })}
           />
+          <p>{errors.email?.message}</p>
         </div>
         <div className="password-signup">
           <label
@@ -123,7 +148,19 @@ const Signup = () => {
             name="password_signup"
             id="password-signup"
             onChange={onChangeInput}
+            {...register("password", {
+              required: "This is required",
+              maxLength: {
+                value: 30,
+                message: "Whoah there! too many characters!",
+              },
+              minLength: {
+                value: 8,
+                message: "Password must at least have 8 characters",
+              },
+            })}
           />
+          <p>{errors.password?.message}</p>
         </div>
         <div className="password-retype">
           <label
@@ -141,12 +178,19 @@ const Signup = () => {
             name="password_retype"
             id="password-retype"
             onChange={onChangeInput}
+            {...register("password_retype", {
+              validate: (value) =>
+                value === password.current || "The passwords do not match",
+            })}
           />
+          <p>{errors.password_retype?.message}</p>
         </div>
         <button type="submit" className="signup-btn">
           Sign up
         </button>
-        <Link to="/" className="has-account">I have an account</Link>
+        <Link to="/" className="has-account">
+          I have an account
+        </Link>
         <ChatCloudSignup className="chat-cloud-signup" />
       </form>
     </main>
