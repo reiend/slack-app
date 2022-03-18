fee2f50327213d55b2dbe5b8114a879fa4ced3c5
jake <loncerasjakerei@gmail.com>
2022-03-18 18:12:25 +0800

    ui for adding channel or friends

--- src/res/components/Chat.jsx
+++ src/res/components/Chat.jsx
@@ -1,35 +1,31 @@
 import React, { useState, useEffect } from "react";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./Chat.scss";
 
 const baseURL = "http://206.189.91.54/api/v1/";
-const Chat = ({ 
-  accessToken,
-  client,
-  expiry,
-  uid,
-}) => {
-
+const Chat = ({ accessToken, client, expiry, uid }) => {
   const navigate = useNavigate();
-  let [channel, setChannel] = useState({
+  const [channel, setChannel] = useState({
     text: [],
     voice: [],
     direct: [],
   });
 
-  const [isExpanded, setIsExpended] = useState(false);
   const [expandedInfo, setExpandedInfo] = useState(false);
+  const [isExpanded, setIsExpended] = useState(false);
+  // state for adding channel or friend
+  const [isAdding, setIsAdding] = useState(false);
 
   const onClickChannelInfoExpand = (evt) => {
     setIsExpended(true);
     const FIRST_WORD = 0;
     const channelType = evt.target.innerText
-      .split(" ")[FIRST_WORD]
-      .toLowerCase();
+      .split(" ")
+      [FIRST_WORD].toLowerCase();
     const channelNames = channel[channelType];
     setExpandedInfo(
-      channelNames.map(name => <li className="channel-name">#{name}</li>)
+      channelNames.map((name) => <li className="channel-name">#{name}</li>)
     );
   };
 
@@ -41,7 +37,15 @@ const Chat = ({
     navigate("/");
   };
 
-  const onMouseChannelInfoExpand = () => setIsExpended(false);
+  const onClickCloseChannelExpand = () => setIsExpended(false);
+
+  // for adding channel and friends
+  const onSubmitAdd = (evt) => {
+    evt.preventDefault();
+    setIsAddingChannel(false)
+  };
+  const onClickAdd = () => setIsAdding(true);
+  const onClickCancelAdd = () => setIsAdding(false);
 
   // temporary fetching of channels
   const getChannels = () => {
@@ -53,21 +57,19 @@ const Chat = ({
         ["client"]: client,
         ["expiry"]: expiry,
         ["uid"]: uid,
-      }
-    })
-    .then(res => {
+      },
+    }).then((res) => {
       const channelsFetch = [];
-      (res.data.data).forEach(data => {
+      res.data.data.forEach((data) => {
         channelsFetch.push(data.name);
-      })
-      setChannel((prevChannel) => ({...prevChannel, text: channelsFetch}));
-    })
+      });
+      setChannel((prevChannel) => ({ ...prevChannel, text: channelsFetch }));
+    });
   };
 
   useEffect(() => {
     getChannels();
   }, []);
-
 
   return (
     <main className="chat">
@@ -76,7 +78,9 @@ const Chat = ({
         <ul className="account-info">
           <li>Friends</li>
           <li>Settings</li>
-          <li id="logout" onClick={onClickSignout}>Signout</li>
+          <li id="logout" onClick={onClickSignout}>
+            Signout
+          </li>
         </ul>
       </nav>
       <div className="chat-room-container">
@@ -98,20 +102,51 @@ const Chat = ({
             ? "channel-info-container"
             : "channel-info-container channel-info-container-expanded"
         }
-        onMouseLeave={onMouseChannelInfoExpand}
       >
-        <nav className={
-          !isExpanded? "channel-info" : "channel-info channel-info-expanded"
-        } ariaLabel="channel-Info">
+        <nav
+          className={
+            !isExpanded ? "channel-info" : "channel-info channel-info-expanded"
+          }
+          ariaLabel="channel-Info"
+        >
           {Object.keys(channel).map((type) => (
             <h2 className="channel-type" onClick={onClickChannelInfoExpand}>
               {type} channel
             </h2>
           ))}
         </nav>
-        { isExpanded && <ul className="channel-name-container"> {expandedInfo} </ul> }
-        { isExpanded && <span className="add-channel">+</span> }
+        {isExpanded && (
+          <ul className="channel-name-container"> {expandedInfo} </ul>
+        )}
+        {isExpanded && (
+          <span
+            className="close-channel-expand"
+            onClick={onClickCloseChannelExpand}
+          >
+            ❌
+          </span>
+        )}
+        {isExpanded && (
+          <span className="add-channel" onClick={onClickAdd}>
+            +
+          </span>
+        )}
       </div>
+        
+        {/* form for adding channels or friends */}
+        {
+        isAdding && 
+        <form className="add" onSubmit={onSubmitAdd}>
+          <div className="input-add-container">
+            <label htmlFor="add-new" className="add-new">New</label>
+            <input type="text" className="input-add" name="add-new" id="add-new"/>
+          </div>
+          <button className="submit-add" type="submit">save</button>
+          <button className="cancel-add" onClick={onClickCancelAdd}>cancel</button>
+        </form>
+        }
+        {/* backdrop for adding channel or friends */}
+        { isAdding && <div className="adding-backdrop"></div>}
     </main>
   );
 };
